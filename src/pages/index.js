5e6039320ed65f04a2794ec34c4f19f75feffa77
Esther Yip <yipjingxin@hotmail.com>
2019-05-27 01:01:28 +0800

    CSS styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <div className="Hero">
       <div className="HeroGroup">
         <h1>Learn to <br />design and code React apps</h1>
+        <Link to="/video">Watch the video</Link>
         <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
         <p>Now go build something great.</p>
         <Link to="/page-2/">Go to page 2</Link>
