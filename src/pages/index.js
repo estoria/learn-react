7c4419869277b927ad7c1a9445fae2564f981294
Esther Yip <yipjingxin@hotmail.com>
2019-05-27 00:46:18 +0800

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to <br />design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
